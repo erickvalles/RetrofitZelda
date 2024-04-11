9a1b324c779a6b026f636701d23f103cbef92b6b
Erick Guerrero <erickg@valles.udg.mx>
2024-04-11 11:39:44 -0600

    Se comenzó el recyclerview

---
+++ app/src/main/java/guerrero/erick/retrofit/ZeldaAdapter.kt
@@ -0,0 +1,22 @@
+package guerrero.erick.retrofit
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import guerrero.erick.retrofit.api.ZeldaItem
+
+class ZeldaAdapter(lista:List<ZeldaItem>):RecyclerView.Adapter<ZeldaHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ZeldaHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ZeldaHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+}
---
+++ app/src/main/java/guerrero/erick/retrofit/ZeldaHolder.kt
@@ -0,0 +1,14 @@
+package guerrero.erick.retrofit
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ZeldaHolder(item: View):RecyclerView.ViewHolder(item) {
+
+    val tvTitulo = item.findViewById<TextView>(R.id.tvTitulo)
+    val ivFondo = item.findViewById<ImageView>(R.id.ivFondo)
+    val tvTipo = item.findViewById<TextView>(R.id.tvTipo)
+
+}
