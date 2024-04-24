09772748b69cb62596b2eff7ed4a0095d9c6e83a
Erick Guerrero <erickg@valles.udg.mx>
2024-04-24 10:48:43 -0600

    Implementación recycler

--- app/src/main/java/guerrero/erick/retrofit/ZeldaAdapter.kt
+++ app/src/main/java/guerrero/erick/retrofit/ZeldaAdapter.kt
@@ -4,7 +4,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import guerrero.erick.retrofit.api.ZeldaItem
 
-class ZeldaAdapter(lista:List<ZeldaItem>):RecyclerView.Adapter<ZeldaHolder>() {
+class ZeldaAdapter(val lista:List<ZeldaItem>):RecyclerView.Adapter<ZeldaHolder>() {
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ZeldaHolder {
         TODO("Not yet implemented")
@@ -15,7 +16,7 @@ class ZeldaAdapter(lista:List<ZeldaItem>):RecyclerView.Adapter<ZeldaHolder>() {
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return lista.size
     }
 
 
